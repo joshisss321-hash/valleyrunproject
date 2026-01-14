30771f1cff03a7041d9ba225ed207bcc692977c3
sonujoshi <joshisss321@gmail.com>
2026-01-14 15:11:09 +0530

    emailfix

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -13,14 +15,14 @@ const sendEmail = async ({ to, subject, html }) => {
     await transporter.sendMail({
       from: `"${process.env.EMAIL_FROM_NAME}" <${process.env.EMAIL_USER}>`,
       to,
-      replyTo: process.env.EMAIL_REPLY_TO || process.env.EMAIL_USER,
+      replyTo: process.env.EMAIL_REPLY_TO,
       subject,
       html,
     });
 
-    console.log("✅ Email sent to:", to);
+    console.log("✅ Email sent:", to);
   } catch (err) {
-    console.error("❌ Email error:", err);
+    console.error("❌ Email error:", err.message);
   }
 };
 
