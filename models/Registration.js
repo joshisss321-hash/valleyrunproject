555e85a930b32461dacd957c6edb0b12caf8b7cc
sonujoshi <joshisss321@gmail.com>
2026-03-20 11:07:11 +0530

    fromsavefix

--- models/Registration.js
+++ models/Registration.js
@@ -1,3 +1,35 @@
+// const mongoose = require("mongoose");
+
+// const registrationSchema = new mongoose.Schema(
+//   {
+//     user: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+
+//     event: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Event",
+//       required: true,
+//     },
+
+//     category: {
+//       type: String,
+//       required: true,
+//     },
+
+//     paymentId: {
+//       type: String,
+//       required: true,
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// module.exports = mongoose.model("Registration", registrationSchema);
 const mongoose = require("mongoose");
 
 const registrationSchema = new mongoose.Schema(
@@ -23,6 +55,18 @@ const registrationSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+
+    // ✅ YE ADD KARO
+    orderId: {
+      type: String,
+    },
+
+    // ✅ YE BHI ADD KARO
+    status: {
+      type: String,
+      default: "paid",
+    },
+
   },
   {
     timestamps: true,
