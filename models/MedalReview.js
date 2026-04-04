f8ca04177ed29a6ad9c94f34a54af114b5b6cf15
sonujoshi <joshisss321@gmail.com>
2026-04-04 16:04:49 +0530

    modelfix

--- models/MedalReview.js
+++ models/MedalReview.js
@@ -1,11 +1,10 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const MedalReviewSchema = new mongoose.Schema({
   name:     { type: String, required: true },
   instaId:  { type: String, required: true },
   review:   { type: String, required: true },
-  imageUrl: { type: String, required: true }, // Cloudinary URL
+  imageUrl: { type: String, required: true },
 }, { timestamps: true });
 
-export default mongoose.models.MedalReview ||
-  mongoose.model("MedalReview", MedalReviewSchema);
+module.exports = mongoose.model("MedalReview", MedalReviewSchema);
